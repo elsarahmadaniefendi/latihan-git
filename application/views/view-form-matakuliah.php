52d16332460e197cc8526fa7e14a2aa417142518
elsarahmadaniefendi <19220326@bsi.ac.id>
2023-10-16 14:59:55 +0700

    Membuat Form Validasi

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -1,33 +1,37 @@
 <html>
+
 <head>
     <title>Form Input Matakuliah</title>
 </head>
+
 <body>
     <center>
         <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
-        <table>
-            <tr>
-                <th colspan="3">
-                    Form Input Data Mata Kuliah
-                </th>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <hr>
-                </td>
-            </tr>
-            <tr>
-                <th>Kode MTK</th>
-                <th>:</th>
-                <td>
-                    <input type="text" name="kode" id="kode">
-                </td>
-            </tr>
-            <tr>
-                <th>Nama MTK</th>
-                <td>:</td>
-                <td>
-                    <input type="text" name="nama" id="nama"></td>
+            <?php echo validation_errors() ?>
+            <table>
+                <tr>
+                    <th colspan="3">
+                        Form Input Data Mata Kuliah
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <hr>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kode MTK</th>
+                    <th>:</th>
+                    <td>
+                        <input type="text" name="kode" id="kode">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Nama MTK</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
                 </tr>
                 <tr>
                     <th>SKS</th>
@@ -50,4 +54,5 @@
         </form>
     </center>
 </body>
+
 </html>
